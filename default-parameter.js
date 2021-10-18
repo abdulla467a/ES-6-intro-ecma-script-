58519b06555ef04fe582b29a26a3579aa09cae78
abdulla467a <ayanrahman009@gmail.com>
2021-10-18 13:42:57 +0600

    comment

--- default-parameter.js
+++ default-parameter.js
@@ -1,12 +1,12 @@
 function add(num1, num2 = 33) {
-    // option 2
+    // // option 2
     // num2 = num2 || 0;
-    // option 1
+    // // option 1
     // if (num2 == undefined) {
     //     num2 = 0;
     // }
-    const total = num1 + num2;
-    return total;
+    // const total = num1 + num2;
+    // return total;
 }
 const result = add(15, 0);
 console.log(result);
--- template-string.js
+++ template-string.js
@@ -15,7 +15,7 @@ const friends = ['abul', 'babul', 'kabul', 'sabul'];
 const count = 5;
 const old = '<h3 class="friend-name">Friend-5</h3>'
 const old2 = '<h3 class="friend-name">Friend-' + count + '</h3>';
-const old2 = `<h3 class="friend-name">Friend-${count}</h3>`;
+// const old2 = `<h3 class="friend-name">Friend-${count}</h3>`;
 const new1 = `<h3 class="friend-name">Friend-${friends.length}</h3>`;
 
 const first = 'Mamun';
